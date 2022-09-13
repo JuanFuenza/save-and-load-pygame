c5308d44f17dd2f6ce74c7901103c25e63d78d00
Juan Fuenzalida <juan.fuenza97@gmail.com>
2022-09-13 11:38:53 -0300

    feat: score save and load from txt in json format

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import json
 
 pygame.init()
 
@@ -7,24 +8,34 @@ HEIGHT = 600
 ds = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Save and load")
 
-score_1 = 0
-score_2 = 0
-
 font = pygame.font.SysFont("Arial", 32)
+
+data = {
+    'red': 0,
+    'blue': 0
+}
+
+try:
+    with open("clicker_score.txt") as score_file:
+        data = json.load(score_file)
+except:
+    print("Not file created yet")
 
 running = True
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            with open('clicker_score.txt', 'w') as score_file:
+                json.dump(data, score_file)
             running = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
             if rect1.collidepoint(mouse_pos):
-                score_1 += 1
+                data['red'] += 1
             if rect2.collidepoint(mouse_pos):
-                score_2 += 1
+                data['blue'] += 1
         
     ds.fill((220, 220, 220))
 
@@ -36,11 +47,11 @@ while running:
     rect2_rect.center = (3*WIDTH//4, HEIGHT//2)
     rect2 = pygame.draw.rect(ds, "blue", rect2_rect)
 
-    score1_text = font.render(f"Clicks: {score_1}", True, "black")
+    score1_text = font.render(f"Clicks: {data['red']}", True, "black")
     score1_rect = score1_text.get_rect(topleft = (WIDTH - 750, 50))
     ds.blit(score1_text, score1_rect)
 
-    score2_text = font.render(f"Clicks: {score_2}", True, "black")
+    score2_text = font.render(f"Clicks: {data['blue']}", True, "black")
     score2_rect = score2_text.get_rect(topleft = (WIDTH - 150, 50))
     ds.blit(score2_text, score2_rect)
 
